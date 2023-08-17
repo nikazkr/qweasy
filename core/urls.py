d296e1dd78c9833824729f29f9fbbc85d894cc06
nikazkr <nikazakariashvili@gmail.com>
2023-08-18 00:11:10 +0400

    create quizz app, with category,quiz, question, answer, favorite, score models. scoring system for individual question when creating quiz. ability to favorite question.
    urls for creating quiz. also creating, selecting, updating, deleting and favoriting questions
    
    added permissions (like teacher/student)

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, Sp
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
+    path('question/', include('quizzes.urls')),
 
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
--- users/urls.py
+++ users/urls.py
@@ -4,12 +4,12 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
-from users.views import GoogleLogin
+from users.views import GoogleLogin, CustomRegisterView
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', include('dj_rest_auth.urls')),
-    path('register/', include('dj_rest_auth.registration.urls')),
+    path('register/', CustomRegisterView.as_view(), name='custom-register'),
     path('login/google/', GoogleLogin.as_view(), name='google_login')
 ]
